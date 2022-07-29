eed63e1395340870861c223b0ec7c4ddecbaa9e3
Frank Naw <franknaw@gmail.com>
2022-07-29 10:57:12 -0400

    Add var validation

--- variables.tf
+++ variables.tf
@@ -17,7 +17,12 @@ variable "gh_org_name" {
 variable "gh_runner_name" {
   description = "The name of github runner."
   type        = string
+  validation {
+    condition     = (contains(["runner1", "runner2"], lower(var.gh_runner_name)))
+    error_message = "The GitHub runner name can only be \"runner1\" or \"runner2\"."
+  }
 }
+
 variable "username" {
   description = "the admin user for the VM"
   type        = string
